093b86b4ce6e2adde6674c4335f7de1034cdd708
Yifeng Wang <yifeng@yeefom.com>
2021-12-29 18:18:05 -0800

    Add more ch3 exercises

---
+++ ch3/ex3_36.cc
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int a1[3] = {1, 2, 3};
+  int a2[3] = {1, 2, 3};
+
+  if (end(a1) - begin(a1) != end(a2) - begin(a2)) {
+    cout << "false";
+  } else {
+    for (int *i = begin(a1), *j = begin(a2); i < end(a1) && j < end(a2); i++, j++) {
+      if (*i != *j) {
+        cout << "false";
+      }
+    }
+
+    cout << "true";
+  }
+
+  return 0;
+}
---
+++ ch3/ex3_45.cc
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr[3][4] = {
+    0, 1, 2, 3,
+    4, 5, 6, 7,
+    8, 9, 10, 11
+  };
+
+  for (auto &row : arr) {
+    for (auto col : row) {
+      cout << col << " ";
+    }
+    cout << endl;
+  }
+
+  cout << endl;
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  cout << endl;
+
+  for (auto row = arr; row < arr + 3; row++) {
+    for (auto col = *row; col < *row + 4; col++) {
+      cout << *col << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
