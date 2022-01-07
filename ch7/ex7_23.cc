db119fdde30d16be73d78c8c5c36d8b865934bc2
Yifeng Wang <yifeng@yeefom.com>
2022-01-06 21:30:25 -0800

    Add move(), set(), display()

--- ch7/ex7_23.cc
+++ ch7/ex7_23.cc
@@ -8,8 +8,8 @@ class Screen {
     using pos = string::size_type;
 
     Screen() = default;
-    Screen(pos width, pos h) : width(width), height(h), content(width * h, ' ') {};
-    Screen(pos width, pos h, pos c) : width(width), height(h), content(width * h, c) {};
+    Screen(pos w, pos h) : width(w), height(h), content(w * h, ' ') {};
+    Screen(pos w, pos h, pos c) : width(w), height(h), content(w * h, c) {};
 
     char get() const {
       return content[cursor];
@@ -19,11 +19,39 @@ class Screen {
       return content[width * row + column];
     }
 
+    Screen &move(pos r, pos c) {
+      cursor = r * width + c;
+
+      return *this;
+    }
+
+    Screen &set(char c) {
+      content[cursor] = c;
+
+      return *this;
+    }
+
+    Screen &display(ostream &os) {
+      do_display(os);
+
+      return *this;
+    }
+
+    const Screen &display(ostream &os) const {
+      do_display(os);
+
+      return *this;
+    }
+
   private:
     pos width = 0;
     pos height = 0;
     pos cursor = 0;
     string content;
+
+    void do_display(ostream &os) const {
+      os << content[cursor];
+    }
 };
 
 int main(int argc, char const *argv[])
